12ab96d40bb1c2051dfb8f0bf847fa7b20ed7e61
rotten99 <mrottboell@gmail.com>
2022-03-29 14:31:07 +0200

    Update Main.java

--- src/com/company/Main.java
+++ src/com/company/Main.java
@@ -4,14 +4,10 @@ public class Main
 {
     public static void main(String[] args)
     {
-    FleetOfCars fleet = new FleetOfCars();
-	ICar yaris= new GasolineCar(20,"YZ31803","Toyota","Yaris",5);
-    ICar tesla= new ElectricCar(100000,648,"XX66666","Tesla","That one",5);
-    ICar duster= new DieselCar(15,true,"ww56389","Dacia","Duster",5);
-    fleet.addCar(yaris);
-    fleet.addCar(tesla);
-    fleet.addCar(duster);
-    System.out.println(fleet);
-    System.out.println("Total rental fee of the cars: "+fleet.totalFee());
+        MainFeature main = new MainFeature();
+        NewFeature feat = new NewFeature();
+        System.out.println(main);
+        System.out.println(feat);
     }
+
 }
