089af4f0578cfd7f467881ac24bbb9160ce40f55
rotten99 <mrottboell@gmail.com>
2022-03-29 14:24:37 +0200

    Added NewFeature.java

---
+++ src/com/company/MainFeature.java
@@ -0,0 +1,11 @@
+package com.company;
+
+public class MainFeature
+{
+    @Override
+    public String toString()
+    {
+        String txt = "This is a main feature";
+        return txt;
+    }
+}
